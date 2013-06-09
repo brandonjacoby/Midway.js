93b5dc891210b71c72f2068dd213758fa4df4b7a
Brandon Jacoby <brandon@brandonjacoby.com>
2013-06-08 19:42:16 -0700

    Midway.js
    Automatically center responsive elements!

---
+++ midway.min.js
@@ -0,0 +1,9 @@
+/**
+ * Midway.js
+ * Version: 1.0
+ * Author: Shipp Co. (Brandon Jacoby, Jordan Singer, Jeremy Goldberg)
+ * Copyright (c) 2013 - Midway.  All rights reserved.
+ * http://www.shipp.co/midway
+ */
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(m).l(5(){4 c=$(\'.0\').1(\'6\',- +$(\'.0\').7()/2);4 d=$(\'.3\').1(\'8\',- +$(\'.3\').9()/2);c;d;$(".0").1({\'h\':\'f\',\'g\':\'e%\',\'i\':\'e%\'});$(j).k(5(){4 a=$(\'.0\').1(\'6\',- +$(\'.0\').7()/2);4 b=$(\'.3\').1(\'8\',- +$(\'.3\').9()/2);a;b})});',23,23,'centerHorizontal|css||centerVertical|var|function|marginLeft|width|marginTop|height|||||50|absolute|top|position|left|window|resize|ready|document'.split('|'),0,{}))
