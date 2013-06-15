acd75f649e4d5e9bfa3efe7c67d0237d58b1c0a8
Brandon Jacoby <brandon@brandonjacoby.com>
2013-06-14 21:26:31 -0700

    Removed unnecessary variables.

--- midway.js
+++ midway.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
-	var midwayH = $('.centerHorizontal').css('marginLeft', - + $('.centerHorizontal').width()/2);
-	var midwayV = $('.centerVertical').css('marginTop', - + $('.centerVertical').height()/2);
 
-	midwayH;
-	midwayV;
+	$('.centerHorizontal').css('marginLeft', - + $('.centerHorizontal').width()/2);
+	$('.centerVertical').css('marginTop', - + $('.centerVertical').height()/2);
 
 	$(".centerHorizontal").css({
 		'position' : 'absolute',
 		'top' : '50%',
 		'left' : '50%'
 	});
+
+	$(".centerVertical").css({
+		'position' : 'absolute',
+		'top' : '50%',
+		'left' : '50%'
+	});
+	
 	
 	$(window).resize(function() {
-		var midwayH = $('.centerHorizontal').css('marginLeft', - + $('.centerHorizontal').width()/2);
-	var midwayV = $('.centerVertical').css('marginTop', - + $('.centerVertical').height()/2);
-
-		midwayH;
-		midwayV;
+		$('.centerHorizontal').css('marginLeft', - + $('.centerHorizontal').width()/2);
+		$('.centerVertical').css('marginTop', - + $('.centerVertical').height()/2);
 	});
 
 });
