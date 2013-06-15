4d57eb0081b298a164f88b9a3d69e7d9147bc307
Brandon Jacoby <brandon@brandonjacoby.com>
2013-06-14 21:49:16 -0700

    Allow triggering of event at any time.

--- midway.js
+++ midway.js
@@ -1,5 +1,4 @@
-$(window).ready(function() {
-
+function Midway() {
 	$('.centerHorizontal').css('marginLeft', - + $('.centerHorizontal').width()/2);
 	$('.centerVertical').css('marginTop', - + $('.centerVertical').height()/2);
 
@@ -14,11 +13,9 @@ $(window).ready(function() {
 		'top' : '50%',
 		'left' : '50%'
 	});
-	
-	
-	$(window).resize(function() {
-		$('.centerHorizontal').css('marginLeft', - + $('.centerHorizontal').width()/2);
-		$('.centerVertical').css('marginTop', - + $('.centerVertical').height()/2);
-	});
+}
 
+$(document).ready(function() {
+	Midway();
+	$(window).on('resize', Midway);
 });
