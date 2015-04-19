1f729cbafe6a21b104f0de598c6b1b50965e285e
christianjuth <cjuth2@gmail.com>
2015-04-18 20:40:16 -0400

    improved order of code to work with bootstrap

--- midway.js
+++ midway.js
@@ -1,23 +1,23 @@
 function Midway(){
-	var $centerHorizontal = $('.midway-horizontal'),
-		$centerVertical = $('.midway-vertical');
+    var $centerHorizontal = $('.midway-horizontal'),
+        $centerVertical = $('.midway-vertical');
 
-	$centerHorizontal.each(function(){
-		$(this).css('marginLeft', -$(this).outerWidth()/2);
-	});
-	$centerVertical.each(function(){
-		$(this).css('marginTop', -$(this).outerHeight()/2);
-	});
-	$centerHorizontal.css({
-		'display' : 'inline',
-		'position' : 'absolute',
-		'left' : '50%'
-	});
-	$centerVertical.css({
-		'display' : 'inline',
-		'position' : 'absolute',
-		'top' : '50%',
-	});
+    $centerHorizontal.css({
+        'display' : 'inline',
+        'position' : 'absolute',
+        'left' : '50%'
+    });
+    $centerVertical.css({
+        'display' : 'inline',
+        'position' : 'absolute',
+        'top' : '50%',
+    });
+    $centerHorizontal.each(function(){
+        $(this).css('marginLeft', -$(this).outerWidth()/2);
+    });
+    $centerVertical.each(function(){
+        $(this).css('marginTop', -$(this).outerHeight()/2);
+    });
 }
 $(window).on('load', Midway);
 $(window).on('resize', Midway);
